5dc6b6a6da8fd2b8d06b241b794d4b976eef9a33
Christopher Jazz Esguerra <christopher.esguerra2@gmail.com>
2026-04-21 14:25:42 +0800

    Display Value

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ function divide(a, b) {
   if (b === 0) return "Error";
   return a / b;
 }
+
+//Logic
+
+function operate(operator, a, b) {
+  if (operator === "+") return add(a, b);
+  if (operator === "-") return subtract(a, b);
+  if (operator === "*") return multiply(a, b);
+  if (operator === "/") return divide(a, b);
+}
+
+//Display value
+function updateDisplay(value) {
+  display.value = value;
+}
